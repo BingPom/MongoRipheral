63684ccd418a3a9871da513e5a9bef097cbe5cf5
Alvaro Serna <alvaro.srnr@gmail.com>
2024-01-26 10:02:59 +0100

    controller y crud planteado

---
+++ src/main/java/controller/PeripheralController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javax.swing.text.Document;
+
+import lombok.NoArgsConstructor;
+import model.Peripheral;
+
+@NoArgsConstructor
+public class PeripheralController {
+	public void insert(Peripheral peripheral) {
+		Document doc;
+	}
+
+	public void delete(String id) {
+		Document doc;
+	}
+
+	public void findAll() {
+
+	}
+
+	public void update(Peripheral peripheral) {
+		Document doc;
+	}
+}
